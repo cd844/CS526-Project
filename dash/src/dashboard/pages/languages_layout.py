0e341a03aba9fe4d611bcf434678d99fc3339d43
cd844 <cd844@scarletmail.rutgers.edu>
2022-04-30 11:42:44 -0400

    filter using time series

--- dash/src/dashboard/pages/languages_layout.py
+++ dash/src/dashboard/pages/languages_layout.py
@@ -3,21 +3,36 @@ from dash import Dash, html, dcc, Input, Output, callback
 import pandas as pd
 import analytics as anal
 import plotly.express as px
+import json
 
 
-# this does not need to run on startup
-
-"""
-Updates language comparisons
-"""
+'''
+update time-range selection
+'''
 @callback(
-    Output('language-comparison-violin', 'figure'),
-    Output('language-comparison-timeseries', 'figure'),
-    Input('language-comparison-dropdown', 'value'),
-    Input('data-all', 'data')
-)
-def update_language_comparison(languages, data_all):
+    Output('time-range-start', 'data'),
+    Output('time-range-end', 'data'),
+    Input('language-comparison-timeseries', 'relayoutData'))
+def display_relayout_data(relayoutData):
+    start_date = None
+    end_date = None
+    if('xaxis.range[0]' in relayoutData and 'xaxis.range[1]' in relayoutData):
+        start_date = relayoutData['xaxis.range[0]']
+        end_date = relayoutData['xaxis.range[1]']
     
+    print(start_date, end_date)
+
+    return start_date, end_date
+
+'''
+plot time series
+'''
+@callback(
+    Output('language-comparison-timeseries', 'figure'),
+    Input('data-all', 'data'),
+    Input('language-comparison-dropdown', 'value'),
+)
+def update_language_time_series(data_all, languages):
     if(not isinstance(languages, list)):
         languages = [languages]
     df = pd.read_json(data_all)
@@ -40,6 +55,29 @@ def update_language_comparison(languages, data_all):
         #print(bins[b])
         time_points = pd.concat([time_points, bins[b]])
     #print(time_points)
+    return px.line(time_points, x=time_label, y = count_label, color = 'language')
+
+"""
+Updates language comparisons
+"""
+@callback(
+    Output('language-comparison-violin', 'figure'),
+    #Output('language-comparison-timeseries', 'figure'),
+    Input('language-comparison-dropdown', 'value'),
+    Input('data-all', 'data'),
+    Input('time-range-start', 'data'),
+    Input('time-range-end', 'data'),
+)
+def update_language_violin(languages, data_all, time_range_start, time_range_end):
+    print('update_language_comparison')
+    print(time_range_start)
+    print(time_range_end)
+    if(not isinstance(languages, list)):
+        languages = [languages]
+    df = pd.read_json(data_all)
+    time_label = 'created_ts'
+    count_label = 'count'
+    lang_label = 'language'
     
     '''
     violin plot code
@@ -56,24 +94,33 @@ def update_language_comparison(languages, data_all):
         violin_points_i[lang_label] = lang
         violin_points_i.index.names = ['df_index']
         violin_points_i = violin_points_i.reset_index()
-        print(violin_points_i)
         violin_points = pd.concat([violin_points, violin_points_i])
         #filtered = filtered.sort_values(by=time_label)
     
-    print(violin_points)
-    return px.violin(violin_points, y = violin_y_label, color = lang_label, box=True, hover_data = ['df_index', 'full_name'], points='all'), px.line(time_points, x=time_label, y = count_label, color = 'language')
+    print(len(violin_points['created_ts']))
+    if(time_range_start != None and time_range_end != None):
+        time_range_start = pd.to_datetime(time_range_start)
+        print(type(time_range_start))
+        time_range_end= pd.to_datetime(time_range_end)
+        print(f"Filter out range [{time_range_start},{time_range_end}]")
+        violin_points['created_ts'] = violin_points['created_ts'].apply(lambda x : pd.to_datetime(x).tz_localize(None))
+        violin_points = violin_points[ violin_points['created_ts'].apply(lambda time : time > time_range_start and time < time_range_end) ]
+    print(len(violin_points['created_ts']))
+    return px.violin(violin_points, y = violin_y_label, color = lang_label, box=True, hover_data = ['df_index', 'full_name', 'created_ts'], points='all')
 
 languages_dropdown = ['Java', 'Python', 'C', 'C++', 'JavaScript', 'PHP', 'Go', "TypeScript", 'CSS', 'React', 'Kotlin', 'Rust']
 layout = html.Div(
         html.Div([
             html.Div(
                 dcc.Dropdown(
-                    languages_dropdown, languages_dropdown[0:2], multi=True,
+                    languages_dropdown, languages_dropdown[0:3], multi=True,
                     id = 'language-comparison-dropdown',
                     className = 'dcc_control'
                 )
             ),
             html.Div(dcc.Graph(id='language-comparison-violin')),
             html.Div(dcc.Graph(id='language-comparison-timeseries')),
+            dcc.Store(id = 'time-range-start'),
+            dcc.Store(id = 'time-range-end'),
         ]),
 )
