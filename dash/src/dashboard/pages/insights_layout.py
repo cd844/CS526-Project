52aff7b7409bfd66cc53dec26a902dcdbfd2ed02
tanup05 <52624008+tanup05@users.noreply.github.com>
2022-05-02 01:54:09 -0400

    markdown text implemented

--- dash/src/dashboard/pages/insights_layout.py
+++ dash/src/dashboard/pages/insights_layout.py
@@ -26,15 +26,15 @@ layout = html.Div([
         dcc.Graph(id = 'bar-lang-vs-count'),
         #html.Div(ddsih.DangerouslySetInnerHTML(load_pie()), className = "pretty_container"),
         dcc.Graph(id = 'pie-lang-vs-count'),
-        html.H5('''This is a languages vs its Count  plot, that shows how frequently each programming languages are used in the projects/ repositories. Javascript is one of the most used languages in most of the repositories followed by CSS.'''),
+        dcc.Markdown('''This is a languages vs its Count  plot, that shows how frequently each programming languages are used in the projects/ repositories. Javascript is one of the most used languages in most of the repositories followed by CSS.'''),
         #html.H3(''' The Pie chart above shows the distribution of all the languages in the entire dataset. It shows the contribution of each language in the data that was scrapped. It looks like Javascript ,HTML and CSS have been the most popular programming languages . '''),
         dcc.Graph(id = 'scatter-lang-vs-watchers'),
-        html.H5(''' The plot above is a languages vs watcher’s count scatter plot that shows how frequently a certain repository that had the above-mentioned language as the highest contributor were viewed. This reflects how  popular a repository is for that particular language. It can be noticed how a repository that made use of c++ the most has the highest watcher’s count followed by JS. This also shows the distribution based on the popularity of each language'''),
+        dcc.Markdown(''' The plot above is a languages vs watcher’s count scatter plot that shows how frequently a certain repository that had the above-mentioned language as the highest contributor were viewed. This reflects how  popular a repository is for that particular language. It can be noticed how a repository that made use of c++ the most has the highest watcher’s count followed by JS. This also shows the distribution based on the popularity of each language'''),
         dcc.Graph(id = 'scatter-lang-vs-fork'),
-        html.H5('''The bar plot about compares how frequently the above-mentioned topic repository was created. It shows that python containing projects were created more than others.
+        dcc.Markdown('''The bar plot about compares how frequently the above-mentioned topic repository was created. It shows that python containing projects were created more than others.
         '''),
         dcc.Graph(id = 'bar-topics-vs-count'),
-        html.H5(''' The languages vs fork_count compares which repositories were interesting based on how frequently they were forked owing to their importance. Repositories in python, JS and Java have been forked more frequently as compared to others. 
+        dcc.Markdown(''' The languages vs fork_count compares which repositories were interesting based on how frequently they were forked owing to their importance. Repositories in python, JS and Java have been forked more frequently as compared to others. 
         '''),
         html.Div(id = 'dummy_input_insights'),
     ],
